a4b01df9f23dbd578c77594d931907437bab5c3f
zhangjiajun <183728895@qq.com>
2025-04-08 15:26:16 +0800

    更新代码格式，修复动画示例，升级版本至0.3.3

--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,48 @@
-import {loadScript} from './utils'
-import packageJson from '../package.json';
+import { loadScript } from './utils';
+// import packageJson from '../package.json';
 
 const Sk = window.Sk;
 const libList = {
-  // "./pgzrun/__init__.js": "./dist/pygame-zero.js",
-  "./pgzrun/__init__.js": `https://cdn.jsdelivr.net/npm/skulpt-pygame-zero@${packageJson.version}/dist/pygame-zero.js`
-}
+  './pgzrun/__init__.js': './dist/pygame-zero.js',
+  //"./pgzrun/__init__.js": `https://cdn.jsdelivr.net/npm/skulpt-pygame-zero@${packageJson.version}/dist/pygame-zero.js`
+};
 export const PyGameZero = {
-  usePyGameZero: function(readFn) {
+  usePyGameZero: function (readFn) {
     const _this = this;
-    return function(file) {
+    return function (file) {
       if (_this.matchModelName(file)) {
-        return _this.load(file)
+        return _this.load(file);
       } else {
-        return readFn.call(this, file)
+        return readFn.call(this, file);
       }
-    }
+    };
   },
-  load: function(file) {
+  load: function (file) {
     return Sk.misceval.promiseToSuspension(
       new Promise((resolve, reject) => {
-        loadScript('https://cdn.bootcdn.net/ajax/libs/pixi.js/5.3.4/pixi.min.js', 'PIXI').then(() => {
-          fetch(libList[file]).then(
-            function (resp){ 
-              const result = resp.text();
-              resolve(result); 
-            }
-          )
-        })
-      }))
+        loadScript(
+          'https://cdn.bootcdn.net/ajax/libs/pixi.js/5.3.4/pixi.min.js',
+          'PIXI'
+        ).then(() => {
+          fetch(libList[file]).then(function (resp) {
+            const result = resp.text();
+            resolve(result);
+          });
+        });
+      })
+    );
   },
-  matchModelName: function(file) {
+  matchModelName: function (file) {
     return Object.keys(libList).includes(file);
   },
-  setContainer: function(el) {
+  setContainer: function (el) {
     this.container = el;
   },
-  reset: function() { 
+  reset: function () {
     this._handleReset();
   },
-  _handleReset: function() {
-  },
-  _onRunning: function(app) {
+  _handleReset: function () {},
+  _onRunning: function (app) {
     this.app = app;
   },
   _moduleCache: {
@@ -49,7 +50,7 @@ export const PyGameZero = {
     timerMap: new Map(),
     soundMap: {},
     music: new Audio(),
-    windowListener: {}
-  }
-}
-window.PyGameZero = PyGameZero
+    windowListener: {},
+  },
+};
+window.PyGameZero = PyGameZero;
